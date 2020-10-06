97bd17a3961f751a1af0b78d7f7c9bcc3d5674f5
nickhir <nick.hirschmueller@yahoo.de>
2020-10-06 21:46:26 +0200

    updated qPCR of D. Weichenhan

--- Dieter Weichenhan/qPCR.R
+++ Dieter Weichenhan/qPCR.R
@@ -4,10 +4,11 @@ library(dplyr)
 
 
 # read in excel file which contains qPCR results (provided by Supervisor)
-excel <- read_excel(path = "data/Course_MCiPqPCR9_9_20.xls")
+setwd("data/")
+excel <- read_excel(path = "Course_MCiPqPCR9_9_20.xls")
 
 # select only columns that are relevant for us (Pos, Sample, Ct)
-# also select rows that are relevant for us. In this case all rows containing A (standard), B(SNRPN) 
+# also select rows that are relevant for us. In this case all rows containing A (standard), B(SNRPN)
 # and H (spike in)
 
 standard <- excel %>%
@@ -20,7 +21,7 @@ gene <- excel %>%
 
 spike <- excel %>%
     select(c("Pos", "Sample", "Ct")) %>%
-    filter(grepl("H\\d+", Pos))
+    filter(grepl("J\\d+", Pos))
 
 # for standard we have to convert column Sample to double instead of chr
 standard$Sample <- as.double(standard$Sample)
@@ -29,65 +30,116 @@ standard$Sample <- as.double(standard$Sample)
 # calculate regression equation
 lm_eqn <- lm(standard$Ct ~ standard$Sample)
 
-metrics <- list(a = format(unname(coef(lm_eqn)[1]), digits = 2),
-                b = format(unname(coef(lm_eqn)[2]), digits = 2),
-                r2 = format(summary(lm_eqn)$r.squared, digits = 3))
+metrics <- list(
+    a = format(unname(coef(lm_eqn)[1]), digits = 2),
+    b = format(unname(coef(lm_eqn)[2]), digits = 2),
+    r2 = format(summary(lm_eqn)$r.squared, digits = 3)
+)
 
-eq <- substitute(italic(C[T]) == a * " "* b  *italic(x)*", "~~italic(R)^2~"="~r2, metrics)
+eq <-
+    substitute(italic(C[T]) == a * " " * b  * italic(x) * ", " ~  ~ italic(R) ^
+                   2 ~ "=" ~ r2,
+               metrics)
 equation <- as.character(as.expression(eq))
 
 
 # plot the standard (row A)
 
-#svg("StandardCurve.svg")
-ggplot(standard, aes(x=Sample, y=Ct))+
-    geom_smooth(method = "lm", se = T)+
-    geom_point()+
-    xlab("Log Concentration")+
-    ylab(bquote(C[T]))+
-    geom_text(x = -0.6, y=26.21, label=equation, parse = T, size=6.7)
-#dev.off()
+# svg("images/StandardCurve.svg")
+ggplot(standard, aes(x = Sample, y = Ct)) +
+    geom_smooth(method = "lm", se = T) +
+    geom_point() +
+    xlab("Log Concentration") +
+    ylab(bquote(C[T])) +
+    geom_text(
+        x = -0.6,
+        y = 26.21,
+        label = equation,
+        parse = T,
+        size = 6.7
+    )
+# dev.off()
 
 # use the regression equation (CT-20.967/-2.945 = Conc) to calculate the log concentration of our gene and spike in
-gene["LogConcentration"] <- (gene$Ct-20.967)/-2.945
+gene["LogConcentration"] <- (gene$Ct - 20.967) / -2.945
 
-spike["LogConcentration"] <- (spike$Ct-20.967)/-2.945
+spike["LogConcentration"] <- (spike$Ct - 20.967) / -2.945
 
 # calculate relative concentrattion
 
-gene["RelativeConcentration"] <- 10^gene$LogConcentration
+gene["RelativeConcentration"] <- 10 ^ gene$LogConcentration
 
-spike["RelativeConcentration"] <- 10^spike$LogConcentration
+spike["RelativeConcentration"] <- 10 ^ spike$LogConcentration
 
 # calculate average for technical duplicates
 
-gene <- gene %>% 
-            group_by(Sample) %>%
-            mutate(AverageRelConc = mean(RelativeConcentration, na.rm = T))
+gene <- gene %>%
+    group_by(Sample) %>%
+    mutate(AverageRelConc = mean(RelativeConcentration, na.rm = T)) %>%
+    mutate(sd = sd(RelativeConcentration, na.rm = T))
 
 spike <- spike %>%
     group_by(Sample) %>%
-    mutate(AverageRelConc = mean(RelativeConcentration, na.rm = T))
+    mutate(AverageRelConc = mean(RelativeConcentration, na.rm = T)) %>%
+    mutate(sd = sd(RelativeConcentration, na.rm = T))
+
+
+
 
 
 
 # remove genotype from sample name and move it to new column
 gene$Sample <- sub("wt\\d+|dko\\d+", "", gene$Sample)
-gene["Genotype"] <- c(rep("WT",10), rep("DKO",10))
+gene["Genotype"] <- c(rep("WT", 10), rep("DKO", 10))
 
 spike$Sample <- sub("wt\\d+|dko\\d+", "", spike$Sample)
-spike["Genotype"] <- c(rep("WT",10), rep("DKO",10))
+spike["Genotype"] <- c(rep("WT", 10), rep("DKO", 10))
+
+# rearrange order of the plot
+gene$Genotype <- factor(gene$Genotype, levels = c("WT", "DKO"))
+
+spike$Genotype <- factor(spike$Genotype, levels = c("WT", "DKO"))
+
+
+
 
 # plot the average Rel concentration for the gene
-ggplot(gene, aes(x=Sample, y=AverageRelConc, fill=Genotype))+
-    geom_bar(stat="identity", position = "dodge")+
-    ylab("Relative Concentration")+
-    scale_fill_manual(values = c("black", "#818181"))
+
+# svg("images/SNRP.svg")
+ggplot(gene, aes(x = Sample, y = AverageRelConc, fill = Genotype)) +
+    geom_errorbar(
+        aes(ymin = AverageRelConc - sd, ymax = AverageRelConc + sd),
+        position = position_dodge(0.45),
+        width = 0.3,
+        size = 1.23
+    ) +
+    geom_bar(stat = "identity",
+             position = position_dodge(),
+             width = 0.45) +
+    ylab("Relative Concentration") +
+    scale_fill_manual(values = c("black", "#818181")) +
+    scale_y_continuous(
+        expand = c(0, 0),
+        limits = c(0, 0.5),
+        breaks = seq(0, 0.5, by = 0.1)
+    )
+# dev.off()
 
 # plot the average Rel concentration for the spike
-ggplot(spike, aes(x=Sample, y=AverageRelConc, fill=Genotype))+
-    geom_bar(stat="identity", position = "dodge")+
-    ylab("Relative Concentration")+
-    scale_fill_manual(values = c("black", "#818181"))
 
+# svg("images/spike.svg")
+ggplot(spike, aes(x = Sample, y = AverageRelConc, fill = Genotype)) +
+    geom_errorbar(
+        aes(ymin = AverageRelConc - sd, ymax = AverageRelConc + sd),
+        position = position_dodge(0.45),
+        width = 0.3,
+        size = 1.23
+    ) +
+    geom_bar(stat = "identity",
+             position = position_dodge(),
+             width = 0.45) +
+    ylab("Relative Concentration") +
+    scale_fill_manual(values = c("black", "#818181")) +
+    scale_y_continuous(expand = c(0, 0), limits = c(0, 2.5))
+# dev.off()
 
