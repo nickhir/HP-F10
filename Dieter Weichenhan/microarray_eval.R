14750c2c66096014cf49c28f4e219f4279747a4d
nickhir <nick.hirschmueller@yahoo.de>
2020-10-12 16:39:05 +0200

    updated Dieter

--- Dieter Weichenhan/microarray_eval.R
+++ Dieter Weichenhan/microarray_eval.R
@@ -4,17 +4,19 @@ library(dplyr)
 # read in excel file which contains array results (provided by Supervisor)
 setwd("data/")
 top5 <- read_excel(path = "IslandArray.xlsx", sheet=1)
+min2 <- read_excel(path = "IslandArray.xlsx", sheet=3)
+island <- read_excel(path = "IslandArray.xlsx", sheet=4)
+promoter <- read_excel(path = "IslandArray.xlsx", sheet=6)
 
 # determine the percentage of regions that are located in genes, promoters or downstream of annotated genes.
 
-inside <- as.numeric(tally(top5, "INSIDE" == CpGLocation)/nrow(top5))
-promoter <- as.numeric(tally(top5, "PROMOTER" == CpGLocation)/nrow(top5))
-downstream <- as.numeric(tally(top5, "DOWNSTREAM" == CpGLocation)/nrow(top5))
+inside <- as.numeric(tally(island, "INSIDE" == CpGLocation)/nrow(island))
+promoter <- as.numeric(tally(island, "PROMOTER" == CpGLocation)/nrow(island))
+downstream <- as.numeric(tally(island, "DOWNSTREAM" == CpGLocation)/nrow(island))
+unknown <- as.numeric(tally(island, "Unknown" == CpGLocation)/nrow(island))
+DIVERGENT_PROMOTER <- as.numeric(tally(island, "DIVERGENT_PROMOTER" == CpGLocation)/nrow(island))
 
-# determine the number of analyzed regions:
+# load in pathway enrichment analysis results
 
-number_DMR <- length(unique(top5$RegNo))
-
-min2 <- read_excel(path = "IslandArray.xlsx", sheet=2)
-
-CGI <- read_excel(path = "IslandArray.xlsx", sheet=3)
+kegg <- read.table("KEGG_Chart.txt", sep = "\t", header=TRUE)
+panther <- read.table("PANTHER_chart.txt", sep = "\t", header=TRUE)
